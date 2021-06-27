cf9b9393bee4c30d1c01961d090fa4902aa8c04e
Kamran Babaev <babaevkamran2094@gmail.com>
2021-06-27 09:12:21 +0300

    5 middle

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Drawer} from "./components/Drawer/Drawer";
 import {Header} from "./components/Header/Header";
 import {Card} from "./components/Card/Card";
 import {useState} from "react";
-import {logDOM} from "@testing-library/react";
+import styles from "./components/Drawer/Drawer.module.scss";
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
     const [items, setItems] = useState([])
     const [cartItems, setCartItems] = useState([])
     const [cartOpened, setCartOpened] = useState(false)
+    const [searchValue, setSearchValue] = useState('')
 
 
     React.useEffect(() => {
@@ -25,12 +26,17 @@ function App() {
             })
     }, [])
 
+    const addAddtoCart = (id) => {
+        setCartItems(prev => [...prev, id])
+    }
 
-    const addAddtoCart = (obj) => {
-        setCartItems(prev => [...prev, obj])
+    const onChangeSearchInput = (event) => {
+        let text = event.currentTarget.value
+        setSearchValue(text)
     }
 
     return (
+
         <div className="wrapper">
 
             {
@@ -45,22 +51,46 @@ function App() {
             <div className='content'>
                 <div className='contentHeader'>
                     <div>
-                        <h1>Все бейсболки:</h1>
+                        <h1>
+                            {
+                                searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все бейсболки: '
+                            }
+                        </h1>
                     </div>
                     <div className='searchBlock'>
                         <img src={'icons/searchICON.png'} width={15} height={15} alt=''/>
-                        <input placeholder='поиск...'/>
+                        {
+                            searchValue ?
+                                <img onClick={() => setSearchValue('')}
+                                     className={styles.removeBTN}
+                                     style={{
+                                         boxSizing: 'border-box',
+                                         outline: 'none',
+                                         position: 'absolute',
+                                         right: '20px'
+                                     }}
+                                     src={'icons/cancel.png'}
+                                     width={12}
+                                     height={12}
+                                     alt=''/>
+                                : null
+                        }
+                        <input onChange={onChangeSearchInput} value={searchValue} placeholder='поиск...'/>
                     </div>
                 </div>
 
                 <div className='allCaps'>
-                    {items.map(obj =>
-                        <Card key={obj.id}
-                              name={obj.name}
-                              price={obj.price}
-                              imgUrl={obj.imgUrl}
-                              onPlus={(obj) => addAddtoCart(obj)}
-                        />)}
+                    {
+                        items
+                            .filter(item => item.name.toLowerCase().includes(searchValue))
+                            .map(obj =>
+                                <Card key={obj.id}
+                                      name={obj.name}
+                                      price={obj.price}
+                                      imgUrl={obj.imgUrl}
+                                      onPlus={(obj) => addAddtoCart(obj)}
+                                />)
+                    }
                 </div>
             </div>
         </div>
